c5fc0aa3f6168a0d10d90b3be6a38a59917d27e6
GytisV <sherman.triplex@gmail.com>
2025-04-02 14:40:51 +0300

    dataisymai

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -5,6 +5,21 @@ class EventController extends BasicCRUDController {
   constructor() {
     super(Model);
   }
+
+  //override generic method
+  // async createItem(req, res) {
+  //   try {
+  //     let inputData = this.constructor.mapInputForCreate(req.body);
+  //     //TODO: add current user id to inputData
+  //     const item = await this.model.create(inputData);
+  //     sendJsonWithToken(res.status(201), {
+  //       [this.fieldNames.singular]: this.constructor.mapOutput(item),
+  //     });
+  //     return;
+  //   } catch (error) {
+  //     sendJsonWithError(res, error);
+  //   }
+  // }
 }
 
 module.exports = EventController;
